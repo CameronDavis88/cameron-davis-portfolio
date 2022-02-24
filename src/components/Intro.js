60a095dda357c2bfe1e1d1929863818abd5ffd29
CameronDavis88 <cammien88@gmail.com>
2022-02-24 15:21:11 -0700

    Deleted unnecessary stuff.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,6 @@
 import profilePic from '../assets/img/source/profile-pic.jpg';
 // import { MdKeyboardArrowDown } from 'react-icons/md';
-import { FaGithub, FaLinkedin, FaRegFileWord, FaFileWord } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaFileWord } from 'react-icons/fa';
 
 const Intro = props => {
     return (
@@ -21,27 +21,6 @@ const Intro = props => {
                     <a href='https://docs.google.com/document/d/1FkFQ68i0zlFsF19WRVv-ZFvTq_aI24--nOWkVFhCTdY/edit?usp=sharing'><FaFileWord size='2.5rem' /><h6>My Resume</h6></a>
                 </div>
             </section>
-            {/* <section className='social-icons tablet'>
-                <div>
-                <a href='https://github.com/CameronDavis88'><FaGithub size='2rem' /></a>
-                <h6>My GitHub</h6>
-                </div>
-                <div>
-                <a href='https://www.linkedin.com/in/cameron-davis-514b8b207/'><FaLinkedin size='2rem' /></a>
-                <h6>My LinkedIn</h6>
-                </div>
-            </section>
-            <section className='social-icons desktop'>
-                <div>
-                <a href='https://github.com/CameronDavis88'><FaGithub size='2.5rem' /></a>
-                <h6>My GitHub</h6>
-                </div>
-                <div>
-                <a href='https://www.linkedin.com/in/cameron-davis-514b8b207/'><FaLinkedin size='2.5rem' /></a>
-                <h6>My LinkedIn</h6>
-                </div>
-            </section> */}
-            {/* <MdKeyboardArrowDown className='section-scroll-down' /> */}
         </section>
     );
 };
