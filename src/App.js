c6a23cbea79cbaa26882db32e396f21112624f97
CameronDavis88 <cammien88@gmail.com>
2022-02-24 13:17:51 -0700

    Cleaned up unnecessary stuff.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-// import Header from './components/Header';
 import Intro from './components/Intro';
 import Projects from './components/Projects';
 import Experience from './components/Skills';
 import Contact from './components/Contact';
-// import Footer from './components/Footer';
 import './styles/App.css';
 import About from './components/About';
 
 function App() {
   return (
     <section className="app-container">
-    {/* <Header /> */}
     <Contact />
     <Intro />
     <About/>
     <Projects />
     <Experience />
-    {/* <Contact /> */}
-    {/* <Footer /> */}
   </section>
   );
 };
